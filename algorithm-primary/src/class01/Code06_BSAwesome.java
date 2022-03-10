1ba0fe08ef8cc579aa99c2e6acedebbc2c6b3005
END <1667932971@qq.com>
2022-03-10 22:17:43 +0800

    Selection,Bubble,BS

---
+++ algorithm-primary/src/class01/Code06_BSAwesome.java
@@ -0,0 +1,83 @@
+package class01;
+
+public class Code06_BSAwesome {
+
+    // arr 相邻的数不相等！
+    public static int getLessIndex(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        if (arr.length == 1 || arr[0] < arr[1]) {
+            return 0;
+        }
+        if (arr[arr.length - 1] < arr[arr.length - 2]) {
+            return arr.length - 1;
+        }
+        int L = 1;
+        int R = arr.length - 2;
+        int mid = 0;
+        while (L < R) {
+            mid = L + ((R - L) >> 1);
+            if (arr[mid] > arr[mid - 1]) {
+                R = mid - 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                L = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return L;
+    }
+
+    // 生成随机数组，且相邻数不相等
+    public static int[] randomArray(int maxLen, int maxValue) {
+        int len = (int) (Math.random() * maxLen);
+        int[] arr = new int[len];
+        if (len > 0) {
+            arr[0] = (int) (Math.random() * maxValue);
+            for (int i = 1; i < len; i++) {
+                do {
+                    arr[i] = (int) (Math.random() * maxValue);
+                } while (arr[i] == arr[i - 1]);
+            }
+        }
+        return arr;
+    }
+
+    // 也用于测试
+    public static boolean check(int[] arr, int minIndex) {
+        if (arr.length == 0) {
+            return minIndex == -1;
+        }
+        int left = minIndex - 1;
+        int right = minIndex + 1;
+        boolean leftBigger = left >= 0 ? arr[left] > arr[minIndex] : true;
+        boolean rightBigger = right < arr.length ? arr[right] > arr[minIndex] : true;
+        return leftBigger && rightBigger;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int maxLen = 100;
+        int maxValue = 200;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = randomArray(maxLen, maxValue);
+            int ans = getLessIndex(arr);
+            if (!check(arr, ans)) {
+                printArray(arr);
+                System.out.println(ans);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+
+    }
+}
